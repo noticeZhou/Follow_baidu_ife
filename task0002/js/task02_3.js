9771ac35951cc4896378edc606a69a7ae63392db
noticeZhou <notice_zhou@163.com>
2016-01-15 17:13:08 +0800

    update rotate

--- task0002/js/task02_3.js
+++ task0002/js/task02_3.js
@@ -1,63 +1,16 @@
-window.onload = function(){
-    setInterval("update()",5000);
-}
-var items = document.getElementsByClassName("item");
-var oneLoop;
-var i=0;
-function pre()    //往左
-{
-    if(i==-1000){
-        clearInterval(oneLoop);
-        var preItem = items[0].parentNode.removeChild(items[0]);  //which is displaying
-        items[0].parentNode.appendChild(preItem);     //push it at the last one        
-        i=0;
-    }
-    else
-        i -= 200;
-    items[0].parentNode.style.marginLeft = i+"px";
-}
+function move(length,time) {
+    var wrap = document.getElementById("wrap");  //图片容器
+    var speed = length/time;
 
-function next()  //往右
-{
-    if(i == 0){
-        i = -1000;
-        clearInterval(oneLoop);
-        var nextItem = items[5].parentNode.removeChild(items[5]);
-        items[0].parentNode.insertBefore(nextItem,items[0]);
-    }
-    else
-        i += 200;
-    items[0].parentNode.style.marginLeft = i+"px";
-}
-
-function custom()
-{
+  
+        wrap_left = parseFloat(wrap.style.marginLeft);
+        console.log(wrap_left,speed);
+        wrap_left -= speed;
+        console.log(wrap_left);
+        wrap.style.marginLeft = wrap_left + "px";
+        setTimeout(move(900,2000),100);
 
 }
-function update()
-{
-    
-    oneLoop = setInterval("next()",200);
-}
 
-// 为element增加一个样式名为newClassName的新样式
-function addClass(element, newClassName) {
-    // your implement
-    try{
-        element.classList.add(newClassName);
-    }catch(ex){
-        oldClassName = element.className;
-        element.className= !oldClassName? newClassName : oldClassName+" "+newClassName;
-    }
-}
-
-// 移除element中的样式oldClassName
-function removeClass(element, oldClassName) {
-    // your implement
-    try{
-        element.classList.remove(oldClassName);  //html5中新增的，classList属性，只有chrome和firefox3.6支持
-    }catch(ex){
-        var re = RegExp("\\b"+oldClassName+"\\b");
-        element.className=element.className.replace(re,"");
-    }
-}
+document.getElementById("wrap").style.marginLeft = "0px";
+setTimeout(move(900,2000),2000);
