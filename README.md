b6c7a05c634db9352d9a7bc1dce55c3ced7d6f34
noticeZhou <notice_zhou@163.com>
2016-01-19 15:43:31 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Follow_baidu_ife
-###用来存放百度前端技术学院的task
+# 跟随baidu_ife的脚步
+###存放百度前端技术学院的task及笔记
+###在线demo
+#### [HTML、CSS基础](http://noticezhou.github.io/Follow_baidu_ife/task0001/index.html) 
+#### [task2 简单输入处理](http://noticezhou.github.io/Follow_baidu_ife/task0002/task02_1.html) 
+#### [task2 倒计时](http://noticezhou.github.io/Follow_baidu_ife/task0002/task02_2.html)
+#### [task2 轮播图](http://noticezhou.github.io/Follow_baidu_ife/task0002/task02_3.html)
+#### [task2 输入提示框](http://noticezhou.github.io/Follow_baidu_ife/task0002/task02_4.html)
+#### [task2 界面脱宅交互](http://noticezhou.github.io/Follow_baidu_ife/task0002/task02_5.html)
--- task0002/js/task02_2.js
+++ task0002/js/task02_2.js
@@ -12,28 +12,31 @@ function compute_time()
 	var date = document.getElementById("date");
     var re = /^\s*(\d{4})-(\d{1,2})-(\d{1,2})/;
     re.exec(date.value);
-    var year = parseInt(RegExp.$1,10);
+    var year = parseInt(RegExp.$1,10);       //输入的年月日
     var month = parseInt(RegExp.$2,10);
     var day = parseInt(RegExp.$3,10);
-
+    date.value = "";       //清空输入框
     var stop = new Date(Date.UTC(year,month-1,day));
     var now = new Date();
+
     var count_down = document.getElementsByClassName("countDown")[0];  //display the countdown
     var ms = stop - now;
 
-    var total = (ms-ms%1000)/1000;
+    var total = (ms-ms%1000)/1000 + now.getTimezoneOffset()*60;  //getTimezoneOffset()获取本地时间与UTC时间相差的分钟数
     if(total <= 0 && count_down){
-    	count_down.innerHTML = "已经过了"+year+"年"+month+"月"+day+"日";
-    	clearTimeout(t);
+    	count_down.innerHTML = "已经过了<strong>"+year+"</strong>年<strong>"+month+"</strong>月<strong>"
+                                +day+"</strong>日";
+    	clearTimeout(t);  //如果超过时间，停止计时
     	return;
     }else if(total <= 0){
     	var time = document.createElement("p");
         time.className = "countDown";
-        time.innerHTML = "已经过了"+year+"年"+month+"月"+day+"日";
+        time.innerHTML = "已经过了<strong>"+year+"</strong>年<strong>"+month+"</strong>月<strong>"
+                                +day+"</strong>日";
         document.getElementsByTagName("form")[0].appendChild(time);
         return;
     }    
-    var s = total % 60;
+    var s = total % 60;  // the total mean the second
     var s_t = (total - s)/60;  //left min
     var m = s_t % 60;
     var m_t = (s_t - m)/60;    //left hour
@@ -41,12 +44,14 @@ function compute_time()
     var d = (m_t - h)/24;    //left day
     
     if(count_down)
-    	count_down.innerHTML = "距离"+year+"年"+month+"月"+day+"日还有"+d+"天"+h+"小时"+m+"分"+s+"秒";
+    	count_down.innerHTML = "距离<strong>"+year+"</strong>年<strong>"+month+"</strong>月<strong>"+day
+    +"</strong>日还有<strong>"+d+"</strong>天<strong>"+h+"</strong>小时<strong>"+m+"</strong>分<strong>"+s+"</strong>秒";
     else{
     	var time = document.createElement("p");
         time.className = "countDown";
-        time.innerHTML = "距离"+year+"年"+month+"月"+day+"日还有"+d+"天"+h+"小时"+m+"分"+s+"秒";
+        time.innerHTML = "距离<strong>"+year+"</strong>年<strong>"+month+"</strong>月<strong>"+day
+    +"</strong>日还有<strong>"+d+"</strong>天<strong>"+h+"</strong>小时<strong>"+m+"</strong>分<strong>"+s+"</strong>秒";
         document.getElementsByTagName("form")[0].appendChild(time);
     }
-    t=setTimeout("compute_time()",1000);
+    t=setTimeout(compute_time,1000);
 }
